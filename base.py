1c4bd30e2ee17b78c64e3b6452567301f04d06a1
charefdz1995 <ccherrad@gmail.com>
2019-05-14 18:03:39 +0100

    new changes to the base module

--- base.py
+++ base.py
@@ -7,6 +7,13 @@ class int_type(Enum):
 	Ethernet = eth 
 
 
+class aceess(object):
+	def __init__(self,address,username,password,secret):
+		self.address = address
+		self.username = username
+		self.password = password
+		self.secret = secret 
+
 class interface(object):
 	def __init__(self,int_type,shelf,slot,port):
 		self.int_type = int_type
@@ -18,14 +25,14 @@ class interface(object):
 
 
 class switch(object):
-	def __init__(self,hostname,interfaces):
+	def __init__(self,hostname,access,interfaces):
 		self.hostname = hostname
 		self.interfaces = interfaces # interface list of the device
-
+		self.access = access
 
 class link(object):
 	def __init__(self,from_int,to_int):
-		self.from_int = from_int
+		self.from_int = from_int # This is a tuple that contain the switch and port
 		self.to_int = to_int
 		self.name = "from " + from_int + " to " + to_int
 
