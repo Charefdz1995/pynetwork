c7f54879ad6bfaa944899d431930b11c8033f586
DJchafie <36542051+DJchafie@users.noreply.github.com>
2019-05-15 16:16:14 +0100

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -6,7 +6,7 @@ interface3 = interface(gi,1,3,ing)
 interface4 = interface(gi,1,4,eg)
 interface5 = interface(gi,1,5,eg)
 interfaces=[interface1,interface2,interface3,interface4,interface5]
-switch1=('R1',interfaces,'192.168.7.1','ansible','ansible','ansible',ing)
+switch1=('R1','192.168.7.1','ansible','ansible','ansible',interfaces,ing)
 file_loader = FileSystemLoader('Templates')
 ENV = Environment(loader=file_loader)
 template = ENV.get_template('Baseline.j2')
