9c099e0f5ac06915d4f15ed096a19af337d68e47
DJchafie <36542051+DJchafie@users.noreply.github.com>
2019-05-15 14:29:58 +0100

    Update base.py

--- base.py
+++ base.py
@@ -5,6 +5,9 @@ class int_type(Enum):
 	GigaEthernet = gi
 	FastEthernet = fa
 	Ethernet = eth 
+class zone_type(Enum):
+        Ingress = ing
+        Egress = eg
 
 
 class aceess(object):
@@ -15,20 +18,21 @@ class aceess(object):
 		self.secret = secret 
 
 class interface(object):
-	def __init__(self,int_type,shelf,slot,port):
+	def __init__(self,int_type,shelf,slot,port,zone_type):
 		self.int_type = int_type
 		#self.shelf = shelf # TODO: moddify this 
 		self.slot = slot
 		self.port = port
 		self.name = int_type + slot + "/"+ port
-
+		self.zone_type = zone_type
 
 
 class switch(object):
-	def __init__(self,hostname,access,interfaces):
+	def __init__(self,hostname,access,interfaces,zone_type):
 		self.hostname = hostname
 		self.interfaces = interfaces # interface list of the device
 		self.access = access
+		self.zone_type = zone_type
 
 class link(object):
 	def __init__(self,from_int,to_int):
